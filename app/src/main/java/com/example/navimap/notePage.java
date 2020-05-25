e66753996983f6e79d6325ab71c5fb54cee702a1
hongwen <hongwenwang7@gmail.com>
2020-05-25 22:42:15 +0800

    calendar修改

--- app/src/main/java/com/example/navimap/notePage.java
+++ app/src/main/java/com/example/navimap/notePage.java
@@ -1,8 +1,11 @@
 package com.example.navimap;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -15,9 +18,12 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class notePage extends AppCompatActivity {
 
@@ -28,6 +34,13 @@ public class notePage extends AppCompatActivity {
     private Intent titleIntent;
     private String saveContent;
     private int id;
+
+    //    Calendar dialog
+    private Dialog calendar;
+    private EditText year, month, day, hour, min, during;
+    private Button calendar_add, calendar_cancel;
+    private Spinner unit;
+    private AlertDialog.Builder add_calendar_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,6 +104,7 @@ public class notePage extends AppCompatActivity {
                         content.clearFocus();
                     }
                     Toast.makeText(getApplicationContext(),"儲存成功",Toast.LENGTH_SHORT).show();
+                    saveContent = content.getText().toString();
                 }
 
             }
@@ -112,8 +126,133 @@ public class notePage extends AppCompatActivity {
         if(item.getItemId() == R.id.action_gallery){
 
         }else if(item.getItemId() == R.id.action_time){
+            if (!saveContent.equals(content.getText().toString())){
+                Toast.makeText(getApplicationContext(),"請先儲存",Toast.LENGTH_SHORT).show();
+                return super.onOptionsItemSelected(item);
+            }
+            init_calendar_dialog();
+            calendar.show();
+            calendar_add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //建立事件開始時間
+                    if(checkEditNull()){
+                        Toast.makeText(getApplicationContext(),"不能有空",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Calendar now = Calendar.getInstance();
+                        int y = Integer.parseInt(year.getText().toString().trim());
+                        if (y < now.get(Calendar.YEAR)) {
+                            Toast.makeText(getApplicationContext(), "時間回不去了", Toast.LENGTH_SHORT).show();
+                            clearEdit(year);
+                        } else if (y >= now.get(Calendar.YEAR) + 100) {
+                            Toast.makeText(getApplicationContext(), "你真的能活這麼久??", Toast.LENGTH_SHORT).show();
+                            clearEdit(year);
+                        } else {
+                            int m = Integer.parseInt(month.getText().toString().trim());
+                            int d = Integer.parseInt(day.getText().toString().trim());
+                            int h = Integer.parseInt(hour.getText().toString().trim());
+                            int minute = Integer.parseInt(min.getText().toString().trim());
+                            int monment = Integer.parseInt(during.getText().toString().trim());
 
+                            Calendar beginTime = Calendar.getInstance();
+                            beginTime.set(y, m - 1, d, h, minute);
+
+                            switch (unit.getSelectedItemPosition()){
+                                case 3:
+                                    y += monment;
+                                    if( y >= now.get(Calendar.YEAR) + 100){
+                                        Toast.makeText(getApplicationContext(),"你活不了這麼久",Toast.LENGTH_SHORT).show();
+                                        clearEdit(during);
+                                        return;
+                                    }
+                                    break;
+                                case 2:
+                                    m += monment;
+                                    break;
+                                case 1:
+                                    d += monment;
+                                    break;
+                                case 0:
+                                    h += monment;
+                                    break;
+
+                            }
+
+                            //建立事件結束時間
+                            Calendar endTime = Calendar.getInstance();
+
+                            endTime.set(y, m - 1, d, h, minute);
+
+                            //建立 CalendarIntentHelper 實體
+                            CalendarIntentHelper calIntent = new CalendarIntentHelper();
+                            //設定值
+                            calIntent.setTitle(title);
+                            calIntent.setDescription(saveContent);
+                            calIntent.setBeginTimeInMillis(beginTime.getTimeInMillis());
+                            calIntent.setEndTimeInMillis(endTime.getTimeInMillis());
+                            //                            calIntent.setLocation("事件地點");
+
+                            //全部設定好後就能夠取得 Intent
+                            Intent intent = calIntent.getIntentAfterSetting();
+
+                            //送出意圖
+                            startActivity(intent);
+
+                            clearEdit("All");
+                            calendar.dismiss();
+                        }
+                    }
+
+                }
+            });
+            calendar_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clearEdit("All");
+                    calendar.dismiss();
+                }
+            });
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void init_calendar_dialog(){
+        calendar = new Dialog(this);
+        calendar.setTitle("Add time!");
+        calendar.setContentView(R.layout.calender_add);
+        year = (EditText)calendar.findViewById(R.id.num_year);
+        month = (EditText)calendar.findViewById(R.id.num_month);
+        day = (EditText)calendar.findViewById(R.id.num_day);
+        hour = (EditText)calendar.findViewById(R.id.num_hour);
+        min = (EditText)calendar.findViewById(R.id.num_min);
+        during = (EditText)calendar.findViewById(R.id.during);
+        unit = (Spinner)calendar.findViewById(R.id.unit);
+        calendar_add = (Button)calendar.findViewById(R.id.calender_add);
+        calendar_cancel = (Button)calendar.findViewById(R.id.calender_cancel);
+    }
+
+    public boolean checkEditNull(){
+        if(year.getText().toString().isEmpty() || month.getText().toString().isEmpty() || day.getText().toString().isEmpty()
+                || hour.getText().toString().isEmpty() || min.getText().toString().isEmpty() || during.getText().toString().isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public void clearEdit(EditText text){
+        text.setText("");
+    }
+
+    public void clearEdit(String all){
+        if(all.equals("All")){
+            year.setText("");
+            month.setText("");
+            day.setText("");
+            hour.setText("");
+            min.setText("");
+            during.setText("");
+        }
+    }
+
 }
